b65f51b906492092caf1450250231ce234eedcef
Srikanth K <srikanthkandarp23@gmail.com>
2023-07-10 17:04:32 +0530

    added icons

---
+++ lib/theme/theme.dart
@@ -0,0 +1,9 @@
+// import 'package:flutter/material.dart';
+
+// class ThemeData {
+//   static ThemeData light() {
+//     return ThemeData(
+//       primarySwatch: Colors.blue,
+//     );
+//   }
+// }
